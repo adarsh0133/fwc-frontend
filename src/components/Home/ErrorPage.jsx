49b5d97b7fa715dff0751d7f6877689cc31ae84a
sunnykurmi <sunnykurmiskks@gmail.com>
2025-03-05 17:11:55 +0530

    developer page created

--- src/components/Home/ErrorPage.jsx
+++ src/components/Home/ErrorPage.jsx
@@ -3,9 +3,11 @@ import React from 'react'
 function ErrorPage() {
   return (
     <div>
-        <div className="w-full h-screen flex flex-col items-center justify-center">
-            <img src="https://i.pinimg.com/originals/db/b8/f9/dbb8f986409cbba56912fa3fc54ee41d.gif" alt="" />
-            <p className='text-5xl uppercase'> 404 Page Not Found</p>
+        <div className="w-full h-screen text-white flex flex-col items-center bg-[#0E3047] justify-center">
+           <img className='w-[40%] max-[600px]:w-full' src="/images/Home/building-webpage.gif" alt="" />
+           <p className='text-4xl translate-y-[-12vh] max-[600px]:translate-y-[0vh]'>Coders At Work</p>
+           <p className='text-2xl  translate-y-[-12vh] max-[600px]:translate-y-[0vh] max-[600px]:mb-5 '>- Coming Soon -</p>
+           <a href="/">Go to Home</a>
         </div>
     </div>
   )
